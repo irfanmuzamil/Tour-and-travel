a5721b9b1a34a6c6ade2c6b2b46cec948a5bb76b
IrfanMuzamil <Irfanullahchagharzai@gmail.com>
2024-06-10 23:41:20 +0500

    travel

---
+++ src/components/Location/Location.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+const Location = () => {
+  return (
+    <>
+      <span id="location"></span>
+      <section data-aos="fade-up" className="">
+        <div className="container my-4">
+          <h1 className="inline-block border-l-8 border-primary/50 py-2 pl-2 mb-4 text-xl font-bold sm:text-3xl">
+            Location to visit our office
+          </h1>
+
+          <div className="rounded-xl ">
+         <iframe width="100%" height="500" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.com/maps?width=100%25&amp;height=200&amp;hl=en&amp;q=Baldia%20Town%20Karachi+(travel%20agency)&amp;t=&amp;z=14&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"><a href="https://www.gps.ie/">gps tracker sport</a></iframe>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default Location;
